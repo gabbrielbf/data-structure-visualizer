ca1ec1161721f836c7db9cf9b9ca01037c1f2690
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-24 15:37:31 -0400

    feat: add conceptual guide file explaining stack and queue theory

---
+++ logica_projeto.py/lessons.py
@@ -0,0 +1,16 @@
+# no arquivo lessons.py
+
+BANNER = """
+======================================================
+    🚀 BEM-VINDO AO SIMULADOR DE PILHAS E FILAS! 🚀
+======================================================
+"""
+
+INTRO_TEXTO = """
+Este programa interativo foi desenvolvido para demonstrar 
+na prática como funcionam duas das estruturas de dados 
+mais importantes na computação:
+
+  📌 PILHAS (Stack - LIFO: Last In, First Out)
+  📌 FILAS  (Queue - FIFO: First In, First Out)
+"""
