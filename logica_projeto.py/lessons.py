280b75e903edc7dca401459514cb787562f434ff
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-24 15:58:57 -0400

    docs: add brief summary with examples for stacks and queues

--- logica_projeto.py/lessons.py
+++ logica_projeto.py/lessons.py
@@ -6,11 +6,35 @@ BANNER = """
 ======================================================
 """
 
-INTRO_TEXTO = """
-Este programa interativo foi desenvolvido para demonstrar 
+INTRO_TEXTO = """Este programa interativo foi desenvolvido para demonstrar 
 na prática como funcionam duas das estruturas de dados 
 mais importantes na computação:
 
   📌 PILHAS (Stack - LIFO: Last In, First Out)
   📌 FILAS  (Queue - FIFO: First In, First Out)
+
+======================================================
+"""
+
+# no arquivo lessons.py
+
+TEORIA_PILHA = """
+======================================================
+--- O QUE É UMA PILHA (STACK)? ---
+Imagine uma pilha de pratos. O último prato que 
+você coloca é o primeiro que você retira 
+(LIFO - Último a Entrar, Primeiro a Sair).
+Exemplo prático: O histórico do navegador 
+ou prório comando 'Desfazer' (Ctrl+Z).
+"""
+DIVISOR = '------------------------------------------------------'
+
+TEORIA_FILA = """
+--- O QUE É UMA FILA (QUEUE)? ---
+Imagine uma fila de banco. A primeira pessoa 
+que chega é a primeira a ser atendida 
+(FIFO - Primeiro a Entrar, Primeiro a Sair).
+Exemplo prático: Fila de impressão 
+ou atendimento de processos.
+======================================================
 """
