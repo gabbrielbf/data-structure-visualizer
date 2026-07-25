852fa81c93be72373da7d4cdae56958d2c2ce312
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-25 17:05:58 -0400

    feat: create main function making the program fully functional and open to improvement suggestions

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-# instanciando e iniciando o programa
+from logica_projeto.estruturas import run_code 
+
 def main():
-    pass
+    """ função que irá rodar o programa para o usuário de forma que ele consiga apenas
+    usar o run sem conseguir usufruir das demais funções e métodos presentes no arquivo "estruturas.py" """
+
+    run_code() # <- não tem mistério, a mágica funciona toda por trás dos panos
+
+    return
 
 if __name__ == '__main__':
     main()
