a60c8549d1e02dbdb0eeed1fe64c414fb75d850c
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-25 17:02:04 -0400

    feat: add __init__ file to main directory to treat it as a package for imports

---
+++ logica_projeto/__init__.py
@@ -0,0 +1,2 @@
+# O arquivo __init__ está completamente vazio, ele serve apenas para avisar o Python que a pasta logica_projeto é um pacote, e assim
+# conseguir rodar todos os arquivos presentes nesta pasta.
