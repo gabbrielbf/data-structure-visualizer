c929aeffe60c430b080023f5335a0b51a6a7b353
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-28 11:04:43 -0400

    refactor: move data handling to classes file and restrict "estruturas" to control management display

--- logica_projeto/classes.py
+++ logica_projeto/classes.py
@@ -7,18 +7,60 @@ class EstruturaDeDados(ABC):
         self._itens = [] # iniciando uma lista vazia na classe pai para que as filhas apenas implementem o modo de lidar com sua respectiva lista
         return
 
+    def exibir_itens(self):
+        if self._itens:
+            print(f'\nExibindo a lista de {self.__class__.__name__}:')
+            print('-'*30)
+            for indice, item in enumerate(self._itens, start=1):
+                print(f'{indice} - {item}')
+            print('-'*30)
+        else:
+            print(f'\nA lista de {self.__class__.__name__} está vazia!\n')
+
     @abstractmethod
     def adicionar_item(self, item):
+        """ método global para adicionar elementos 
+            independente do tipo de lista """
         pass
 
     @abstractmethod
     def remover_item(self):
+        """ método responsável pela remoção dos itens gerais, independente se é PILHA ou FILA pois 
+        o que definirá a remoção será o a própria classe em si instânciada no programa procedual """
         pass
 
 
-class Pilha(EstruturaDeDados):
-    pass
+class Pilhas(EstruturaDeDados):
+
+    def adicionar_item(self, item):
+        self._itens.append(item)
+        return 
+
+    def remover_item(self):
+        if self._itens:
+            print('-'*30)
+            print('Pilha (LIFO – Last In, First Out / O último que entra é o primeiro a sair)')
+            print(f'Removemos o último item: {self._itens[-1]}')
+            print('-'*30)
+            self._itens.pop()
+        else:
+            print(f'\nA lista de {self.__class__.__name__} está vazia!\n')
+        return 
 
 
-class Fila(EstruturaDeDados):
-    pass
+class Filas(EstruturaDeDados):
+
+    def adicionar_item(self, item):
+        self._itens.append(item)
+        return 
+
+    def remover_item(self):
+        if self._itens:
+            print('-'*30)
+            print('Fila (FIFO – First In, First Out / O primeiro que entra é o primeiro a sair)')
+            print(f'Removemos o primeiro item: {self._itens[0]}')
+            print('-'*30)
+            self._itens.pop(0)
+        else:
+            print(f'\nA lista de {self.__class__.__name__} está vazia!\n')
+        return 
--- logica_projeto/estruturas.py
+++ logica_projeto/estruturas.py
@@ -1,6 +1,6 @@
 # estrutura dos dados
 from .menu import menu_numerado, limpar_tela, exibir_pilhas_ou_filas, selecionar_pilhas_ou_filas, retornar_formatado # { usamos um "." depois do "from" para conseguir importar essas funções e métodos
-from .classes import Pilha, Fila # para o arquivo "main" e assim fazer com que ele funcione 
+from .classes import Pilhas, Filas # para o arquivo "main" e assim fazer com que ele funcione 
 from .lessons import * # } da forma mais enxuta possível, exibindo ao usuário somente o que é necessário
 
 import time
@@ -9,10 +9,6 @@ import sys
 def obter_elemento():
     """ Função criada apenas para deixar o 
     médodo 1 da função "run_code" mais "enxuto" """
-
-    pilha_ou_fila.exibir_pilhas_ou_filas()
-    retorno = pilha_ou_fila.selecionar_pilhas_ou_filas()
-    pilha_e_fila = pilha_ou_fila.retornar_formatado()
 
     while True:
         entrada = input('Qual elemento deseja adicionar: ')
@@ -23,7 +19,7 @@ def obter_elemento():
             print(f'\nO elemento [{entrada}] foi adicionado a lista de {pilha_e_fila} ✔️\n')
             break
 
-    return entrada, retorno
+    return entrada
 
 def lecionar_usuario():
     """ exibição dinâmica e animada para 
@@ -57,65 +53,6 @@ def lecionar_usuario():
     print(DIVISOR)
 
     return
-
-def adicionar_elemento(elemento, retorno):
-    """ função global para adicionar elementos 
-    independente do tipo de lista """
-
-    if retorno == 1:
-        lista_pilhas.append(elemento) # <- Joga na pilha se o retorno for 1
-    else:
-        lista_filas.append(elemento) # <- Joga na fila se for outro valor (no caso 2)
-        
-    return
-
-def remover_elemento():
-    """ função responsável pela remoção dos itens gerais, independente se é PILHA ou FILA pois o que definirá a remoção
-    será o retorno da variavel "retorno" presente no método da classe "selecionar_pilhas_ou_filas" """
-
-    if retorno == 1:
-        if lista_pilhas:
-            print('-'*30)
-            print('Pilha (LIFO – Last In, First Out / O último que entra é o primeiro a sair)')
-            print(f'Removemos o último item: {lista_pilhas[-1]}')
-            print('-'*30)
-            lista_pilhas.pop()
-        else:
-            print(f'\nA lista de {pilha_e_fila} está vazia!\n') # <- variável "pilha_e_fila" criada apenas para exibir dinâmicamente qual lista está
-    else:                                                       # sendo manipulada em tempo real
-        if lista_filas:
-            print('-'*30)
-            print('Fila (FIFO – First In, First Out / O primeiro que entra é o primeiro a sair)')
-            print(f'Removemos o primeiro item: {lista_filas[0]}')
-            print('-'*30)
-            lista_filas.pop(0)
-        else:
-            print(f'\nA lista de {pilha_e_fila} está vazia!\n')
-
-    return
-
-def exibir_elementos():
-    """ função responsável pela exibição dos itens
-    gerais seguindo a mesma lógica da função de remoção """
-
-    if retorno == 1:
-        if lista_pilhas:
-            print(f'\nExibindo a lista de {pilha_e_fila}:')
-            print('-'*30)
-            for indice, item in enumerate(lista_pilhas, start=1):
-                print(f'{indice} - {item}')
-            print('-'*30)
-        else:
-            print(f'\nA lista de {pilha_e_fila} está vazia!\n')
-    else:
-        if lista_filas:
-            print(f'Exibindo a lista de {pilha_e_fila}:')
-            print('-'*30)
-            for indice, item in enumerate(lista_filas, start=1):
-                print(f'{indice} - {item}')
-            print('-'*30)
-        else:
-            print(f'\nA lista de {pilha_e_fila} está vazia!\n')
 
     return
 
@@ -153,9 +90,9 @@ def definir_estrutura():
     opcao = selecionar_pilhas_ou_filas()
 
     if opcao == 1:
-        estrutura_atual = Pilha()
+        estrutura_atual = Pilhas()
     else:
-        estrutura_atual = Fila()
+        estrutura_atual = Filas()
 
     nome_formatado = retornar_formatado(opcao)
 
@@ -163,9 +100,6 @@ def definir_estrutura():
     print(f'Iremos trabalhar com {nome_formatado}')
 
     return estrutura_atual
-
-lista_pilhas = []
-lista_filas = []
 
 def run_code():
     """ função responsável por pegar todas as funções definidas acima e fazê-las funcionar em conjunto com
@@ -181,16 +115,19 @@ def run_code():
 
         match opcao:
             case 1:
-                elemento, retorno = obter_elemento()
-                adicionar_elemento(elemento, retorno)
+                elemento = obter_elemento()
+                estrutura_atual.adicionar_item(elemento)
             case 2 :
-                remover_elemento()
+                item_removido = estrutura_atual.remover_item()
+                print(f'Item removido: [{item_removido}]')
             case 3:
-                exibir_elementos()
+                estrutura_atual.exibir_itens()
             case 4:
+                pass
+            case 5:
                 if encerrar_programa() == True:
                     break
                 else:
                     continue
-                
+
     return
