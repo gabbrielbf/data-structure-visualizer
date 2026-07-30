4022e29f5172095a51c5d164fa83a31a82a380c7
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-30 14:38:59 -0400

    feat(database): add method to load lists with error handling for corrupted or empty data

--- src/database.py
+++ src/database.py
@@ -22,3 +22,18 @@ class DatabaseManager:
             json.dump(data, file, indent=4) # <- this block opens the file in write mode and dumps the dictionary into json format
 
         print(f'\n[INFO] Data succssufully saved to {self._filename} ✔\n')
+
+    def load_data(self):
+        """ loads data from the json file if it exists
+        returing a dictionary with the items """
+
+        if not os.path.exists(self._filename): # <- checks if the file exists before trying to read it
+            return {'stacks': [], 'queues': []}
+
+        # opens the file in read mode and loads the json content back to python
+        with open(self._filename, 'r', encoding='utf-8') as file:
+            try:
+                data = json.load(file)
+                return data
+            except json.JSONDecodeError: # <- handles corrupted or empty json files gracefully
+                return {'stacks': [], 'queues': []}
