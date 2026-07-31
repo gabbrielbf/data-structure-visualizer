53e65716b2fa242bee6b0a69b130e464a45d96fb
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-31 10:34:52 -0400

    test(tests): add test function for stack functionality

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,1 +1,21 @@
 from src.classes import Stacks, Queues
+
+def test_stack_behavior():
+    """ tests if the stack follows the LIFO 
+    (Last In, First Out) principle correctly """
+
+    stack = Stacks() # <- # creates an empty stack instance
+
+    stack.add_item('Pineapple') # { adds elements 
+    stack.add_item('Bread') # } to the stack
+
+    # checks if the last item added is at the top (index -1)
+    assert stack._items[-1] == 'Bread'
+
+    # simulates removing the last item (pop)
+    stack._items.pop()
+
+    # checks if the remaining item is now Apple
+    assert stack._items[-1] == 'Pineapple'
+    return
+
