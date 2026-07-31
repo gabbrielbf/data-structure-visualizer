46a1288e92ca0e7da6ffd5e5d1025ff124612c0f
gabbrielbf <tiagogabbrielzx47@gmail.com>
2026-07-31 10:47:33 -0400

    test(tests): add test function for queue functionality

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -17,5 +17,26 @@ def test_stack_behavior():
 
     # checks if the remaining item is now Apple
     assert stack._items[-1] == 'Pineapple'
+
+    return
+
+def test_queue_behavior():
+    """ tests if the queue follows the FIFO 
+    (First In, First Out) principle correctly """
+
+    # The code below is similar to Stacks, so in case of doubt, 
+    # just read the comments of the function above.
+
+    queue = Queues()
+
+    queue.add_item('First')
+    queue.add_item('Second')
+
+    assert queue._items[0] == 'First'
+
+    queue._items.pop(0)
+
+    assert queue._items[0] == 'Second'
+
     return
 
